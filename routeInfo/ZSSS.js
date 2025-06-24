da578701821e5e49b7fcd4ac399a2d8e86ecca0f
xloalx <loal@yurnga.com>
2025-06-24 12:13:18 +0800

    Update ZSSS.js

--- routeInfo/ZSSS.js
+++ routeInfo/ZSSS.js
@@ -1,1 +1,56 @@
 
+export const ZSSS = {
+    route: {
+        outbound: "VHHH LEKEN1B LEKEN V601 DOTMI A470 LJG B221 SHZ G204 JTN AND61A
+ZSSS",
+        inbound: "ZSSS NXD71D NXD W131 AKDIM A470 DOTMI V512 ABBEY ABBEY4A VHHH",
+    },
+    radio: {
+        outbound: {
+            DEP: {
+                GND: "121.875",
+                GND2: "122.6",
+                TWR: "118.4",
+                DEP: "123.8",
+                DEP2: "133.825",
+            },
+            DALOL: "HKG 118.925",
+            DOTMI: "SHA 128.35",
+            TEBON: "SHA 120.9",
+            ENVEN: "SHA 128.15",
+            UBTIX: "SHA 132.625",
+            DST: "SHA 128.75",
+            ARR: {
+                APP: "119.975",
+                FL180: "125.85",
+                APP1: "120.3",
+                APP2: "126.65"  
+				        TWR: "ZSSS 118.1",
+                GND: "121.9",
+                APN: "121.675",
+            }
+        },
+        inbound: {
+            DEP: {
+                APN: "121.675",
+                GND: "121.9",
+                TWR: "118.1",
+                TWR2: "",
+                DEP: "",
+                
+            },
+            MORE: "",
+            DOTMI: "HKG 121.3",
+            MAGOG: "HKG RAD 126.5",
+            ABBEY: "HKG ARR",
+            ARR: {
+                ARR: "119.1",
+                DIR: "119.5",
+                TWR: "118.7",
+                GND: "122.6",
+                GND2: "121.875",
+            }
+        }
+    }
+};
+
