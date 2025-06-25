4e9359b673e1b3a56354806433938e98b1bf19d7
xloalx <loal@yurnga.com>
2025-06-25 21:52:27 +0800

    Create RJAA.js

---
+++ routeInfo/RJAA.js
@@ -0,0 +1,56 @@
+export const RJAA = {
+    route: {
+        outbound: "DALOL V631 ENVAR M750 MADOG Y53 NIXOV",
+        inbound: "OLBUR Y236 IVTER Y34 HKC Y50 IGMON A1 ELATO V522 ABBEY"
+    },
+   
+    radio: {
+        outbound: {
+         
+            DEP: {
+                GND: "121.875",
+                GND2: "122.6",
+                TWR: "118.4",
+                DEP: "123.8",
+                DEP2: "133.825"
+            },
+            DALOL: "HKG 118.925",
+            ENVAR: "TPE 129.1",
+            TONGA: "TPE 126.7",
+            KUDOS: "TPE 123.6",
+            MOLKA: "FUK 127.5",
+            INVAM: "FUK 135.3",
+            MIDAI: "KOBE 133.85",
+            
+            ARR: {
+                APP: "",
+                RAD: "",
+                TWR: "",
+                GND: ""
+            }
+        },
+        inbound: {
+            DEP: {
+                GND: "",
+               GND2: "",
+               TWR: "",
+                KIX: "D"
+            },
+         
+            JEDAI: "FUK 127.5",
+            BULAN: "TPE 123.6",
+            APU: "TPE 126.7",
+            MKG: "TPE 129.1",
+            ELATO: "HKG 121.3",
+            MAGOG: "HKG RAD 126.5",
+            ABBEY: "HKG ARR",
+            ARR: {
+                ARR: "119.1",
+                DIR: "119.5",
+                TWR: "118.7",
+                GND: "122.6",
+                GND2: "121.875",
+            }
+        }
+    }
+};
