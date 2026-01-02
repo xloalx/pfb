88e27ca806066c735618d58e275b9139f8c7496a
xloalx <loal@yurnga.com>
2026-01-02 20:50:52 +0800

    Renamed YMML.js to YSSY.js retaining content for YSSY

---
+++ YSSY.js
@@ -0,0 +1,46 @@
+export const YSSY = {
+    route: {
+        outbound: "DALOL V641 NOMAN A461 AVMUP W16 MIA DCT LIGPA B472 COT R590 AMN R340 SURVO Y161 OVNOM H652 BOREE",
+        inbound: " Flight Plan",
+    },
+    radio: {
+        outbound: {
+            DEP: {
+                GND: "121.875",
+                GND2: "122.6",
+                TWR: "118.4",
+                DEP: "123.8",
+                DEP2: "132.15"
+            },
+            NOMAN: "PDC RPHI",
+            ENVAR: "TPE 129.1",
+            OTHER: "",
+            ARR: {
+                APP: "",
+				TWR: "",
+                GND_W: "",
+                GND_E: ""
+            }
+        },
+        inbound: {
+            DEP: {
+                GND_E: "",
+                GND_W: "",
+                TWR: "",
+                DEP: "",
+                
+            },
+            MORE: "",
+            ELATO: "HKG 121.3",
+            MAGOG: "HKG RAD 126.5",
+            ABBEY: "HKG ARR",
+            ARR: {
+                ARR: "119.1",
+                DIR: "119.5",
+                TWR: "118.7",
+                GND: "122.6",
+                GND2: "121.875",
+            }
+        }
+    }
+}.
