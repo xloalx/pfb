fc1db06bc649e3b226e10d62623ac19aa1e088e1
Adam Lowe <adam.lowe@dodaraviation.com>
2026-01-03 17:56:57 +0800

    location
    fix location

--- routeInfo/yssy.js
+++ routeInfo/yssy.js
@@ -1,1 +1,74 @@
-Placeholder content as exact file details are unavailable.
+export const YSSY = {
+    route: {
+        outbound: "DALOL V641 NOMAN A461 AVMUP W16 MIA DCT LIGPA B472 COT R590 AMN R340 SURVO Y161 OVNOM H652 BOREE",
+        inbound: " Flight Plan",
+    },
+    radio: {
+        outbound: {
+            DEP: {
+                GND: "121.875",
+                GND2: "122.6",
+                TWR: "118.4",
+                DEP: "123.8",
+                DEP2: "132.15"
+            },
+            AKOTA: "HKG 132.7"
+            NOMAN_PDC: "RPHI",
+            NOMAN: "MNL 119.3",
+            HF MNL: "8945/5655",
+            AVMUP: "MNL 120.5",
+            OLIVA: "MNL 125.7",
+            MARAN: "MNL 124.9",
+            PEDNO_PDC: "WAAF",
+            PEDNO: "UJN 128.1",
+            DULON: "UJN 132.35",
+            GUTEV: "BNE 6556/11386",
+            BNE_PDC: "YBBB",
+            IGOPO: "BNE 133.7",
+            DUMAV: "BNE 135.7",
+            ABM_YDMG: "BNE 125.2",
+            ABM_YOSB: "BNE 134.2",
+            MEYJI: "BNE 133.8",
+            OTLAM: "BNE 133.0",
+            IDOGI: "BME 123.4",
+            ARR: {
+                APP: "",
+				TWR: "120.5",
+                GND: "126.5",
+                GND_E: ""
+            }
+        },
+        inbound: {
+            DEP: {
+                GND: "126.5",
+                TWR: "120.5",
+                DEP: "129.7",
+                DEP2: "135.9",
+                
+            },
+            FL100_PDC; "YBBB",
+            FL180: "BNE 133.0",
+            POTUM: "BNE 133.4",
+            IVRAD: "BNE 134.2",
+            RENEK: "BNE 133.0",
+            GULOP: "BNE 135.7 / 135.3",
+            VEVAV: "BNE 135.3 / 133.7",
+            UJN_PDC: "WAAF",
+            OPABA: "UJN 133.1",
+            SAMBE: "UJN 128.1",
+            ELNIT: "MNL 132.5",
+            SAGAM: "MNL 125.7",
+            PABLO: "MNL 128.7",
+            AVMUP: "MNL 119.3",
+            NOMAN: "HKG 132.15",
+            ARR: {
+                CTR: "126.3",
+                ARR: "119.1",
+                DIR: "119.5",
+                TWR: "118.7",
+                GND: "122.6",
+                GND2: "121.875",
+            }
+        }
+    }
+}.
